0217c857fa422d7aaed39b20ef175b4f518b17de
Rafał Babski <rbabski@n7space.com>
2021-08-26 10:12:03 +0200

    Fixed code formatting

--- src/broker_lock.cpp
+++ src/broker_lock.cpp
@@ -2,20 +2,12 @@
 
 #include "lock.h"
 
-namespace
-{
-	taste::Lock broker_lock;
+namespace {
+taste::Lock broker_lock;
 }
 
-extern "C"
-{
-	void Broker_acquire_lock()
-	{
-		broker_lock.lock();
-	}
+extern "C" {
+void Broker_acquire_lock() { broker_lock.lock(); }
 
-    void Broker_release_lock()
-	{
-		broker_lock.unlock();
-	}
+void Broker_release_lock() { broker_lock.unlock(); }
 }
--- src/broker_lock.h
+++ src/broker_lock.h
@@ -1,10 +1,9 @@
 #ifndef BROKER_LOCK_H
 #define BROKER_LOCK_H
 
-extern "C"
-{
-	void Broker_acquire_lock();
-    void Broker_release_lock();
+extern "C" {
+void Broker_acquire_lock();
+void Broker_release_lock();
 }
 
 #endif
