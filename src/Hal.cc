beaed5d6bbc0b216ea8f39ee8adf30d5b0be8051
Filip Florczyk <filip.florczyk@n7space.com>
2025-07-24 13:48:19 +0200

    Pre-review fix

--- src/Hal.cc
+++ src/Hal.cc
@@ -1,15 +1,23 @@
 /**@file
- * This file is part of the TASTE Leon3 Runtime.
+ * This file is part of the TASTE Linux Runtime.
  *
  * @copyright 2025 N7 Space Sp. z o.o.
  *
- * Leon3 Runtime is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * TASTE Linux Runtime was developed under a programme of,
+ * and funded by, the European Space Agency (the "ESA").
  *
- * You should have received a copy of the GNU General Public License
- * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the ESA Public License (ESA-PL) Permissive,
+ * Version 2.3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "Hal.h"
